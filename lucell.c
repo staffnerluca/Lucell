ae78bdfb24e884ccc744a391e867c9eb9cb55e92
Luca Staffner <staffnerluca@gmail.com>
2023-09-18 15:26:06 +0200

    added List of Keywords, single Char Tokens and two char tokens

--- lucell.c
+++ lucell.c
@@ -15,6 +15,53 @@ typedef struct Token{
 	char *text;
 } Token;
 
+char* OneCharTokens = "("
+			")"
+			"{"
+			"}"
+			"["
+			"]"
+			";"
+			"+"
+			"-"
+			"*"
+			"/"
+			"%"
+			"=";
+
+char* TwoCharTokens = "!="
+			"=="
+			">="
+			"<=";
+
+char* KeyWords = "data"
+		"if" 
+		"then" 
+		"else" 
+		"case" 
+		"of" 
+		"where" 
+		"let" 
+		"in"
+    		"do" 
+		"module" 
+		"import" 
+		"qualified" 
+		"as"
+    		"class" 
+		"instance" 
+		"deriving" 
+		"type" 
+		"newtype"
+    		"default" 
+		"deriving" 
+		"family" 
+		"deriving" 
+		"instance" 
+		"deriving" 
+		"class" 
+		"deriving";
+
 int main(int argc, char *argv[]){
 	if(argc == 1){
 		printf("Error: File name expected");
@@ -50,7 +97,29 @@ int checkIfFileExists(char path[]){
 	}
 	return exists;
 }
- 
+
+
+node* createTokensFromFile(char path[]){
+    FILE* file = fopen("example.txt", "r");
+
+    if (file == NULL) {
+        perror("Failed to open the file");
+    }
+    //fget retrns a int not a char!!!
+    int c;
+    while ((c = fgetc(file)) != EOF) {
+        // 'c' contains the character read from the file
+        // You can process or print it as needed
+	char ch = (char)c;
+
+        
+    }
+
+    // Close the file
+    fclose(file);
+
+}
+
 //scanner reads every char of the file and creates tokens for example string hello = "Hello"; 1 string 2. hello 3. = 4. "Hello" 5. ;
 //check if naming of variabel is correct by regular Expression: [a-zA-Z_][a-zA-Z_0-9]*
 //returns Token List
